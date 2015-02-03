9a2ff2594381d239e0b768bf098fd3ce4d5d32f4
Steve Kinney <hello@stevekinney.net>
2015-02-03 06:41:15 -0700

    Add customizations for Markdown, Ruby, Web

---
+++ customizations/setup-markdown.el
@@ -0,0 +1,8 @@
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.mdown$" . markdown-mode))
+(add-hook 'markdown-mode-hook
+          (lambda ()
+            (visual-line-mode t)
+            (writegood-mode t)
+            (flyspell-mode t)))
+(setq markdown-command "pandoc --smart -f markdown -t html")
